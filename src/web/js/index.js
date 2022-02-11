e1b7a2e96680dcee6f0820821180a0de5cefc4ab
Léandro Laurent--Aquino <laurentaquino.leandro@gmail.com>
2022-02-11 17:18:52 +0100

    Better historic display

--- src/web/js/index.js
+++ src/web/js/index.js
@@ -7,8 +7,16 @@ I'will (or if you want to) improve the "return result" system
 Beacause 'alert' is a very ugly way to do it xD
 */
 
-btn.onclick = function(notTooMuchCaheForChrome) {
+
+const historicDiv = document.querySelector('.historic');
+
+btn.onclick = function() {
     const inputOpValueRaw = document.getElementById('input-op').value;
     let parsedValue = eval(inputOpValueRaw);
-    alert(parsedValue);
+    // alert(parsedValue);
+    if (inputOpValueRaw == false) { // more readable than !inputOpValueRaw
+        alert("Please enter a value before submit");
+    } else {
+        historicDiv.innerHTML += "<br>" + inputOpValueRaw + "<br>" + "---------------------" + "<br>" + parsedValue + "<br>";
+    }
 };
